5af9623fee9b64ef76047c3fe39eff322357435e
Steve Brownlee <chortlehoort@gmail.com>
2025-04-30 12:19:38 -0400

    Better nav layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import "./Navbar.css"
 export const NavBar = () => {
     const navigate = useNavigate()
     return (
-        <ul className="navbar pb-10">
+        <ul className="navbar pb-10 flex justify-between">
             <li className="navbar__item pl-10">
                 <NavLink className="text-left underline text-blue-600 hover:text-purple-700" to={"/allrocks"}>All Rocks</NavLink>
             </li>
